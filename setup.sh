07b1ce10977c1faa3765aa1022027eaed9e7728b
cqsd <>
2018-05-21 19:32:12 -0700

    better install script

--- setup.sh
+++ setup.sh
@@ -1,23 +1,57 @@
 #!/bin/bash
+srcdir=$(pwd)
 
-function linkdotfile {
-    sourcedir=$(pwd)
-    filename=$1
-    cd ~/ > /dev/null
+vimplugins=(
+    Rip-Rip/clang_complete
+    kien/ctrlp.vim
+    Shougo/deoplete.nvim
+    eagletmt/ghcmod-vim
+    neovimhaskell/haskell-vim
+    eagletmt/neco-ghc
+    Shougo/neocomplete.vim
+    scrooloose/nerdtree
+    edkolev/promptline.vim
+    luochen1990/rainbow
+    scrooloose/syntastic
+    bling/vim-airline
+    vim-airline/vim-airline-themes
+    guns/vim-clojure-static
+    altercation/vim-colors-solarized
+    tpope/vim-fireplace
+    dag/vim-fish
+    tpope/vim-fugitive
+    w0ng/vim-hybrid
+    kristijanhusak/vim-hybrid-material
+    henrik/vim-indexed-search
+    tpope/vim-surround
+    Shougo/vimproc.vim
+)
 
-    # idk why but -f doesn't work for me?
-    if [[ -f ~/$filename && ! -L ~/$filename ]]; then
-        echo "$filename already exists; backing up as $filename.bk."
-        mv ~/$filename ~/$filename.bk
-    fi
+echo About to nuke your ~/.vim/bundle
+read -r -p "Is that OK? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        rm -fr ~/.vim/bundle
+        mkdir -p ~/.vim/bundle
+        pushd ~/.vim/bundle
+        echo ${vimplugins[*]} | xargs -n1 | lam -s 'https://github.com/' - | xargs -n1 -P8 git clone
+        popd
+        ;;
+    *)
+        echo skipping vim plugins
+        ;;
+esac
 
-    {
-        ln -sf $sourcedir/$filename $filename &&
-        echo "Linked $filename to ~/$filename."
-    } || echo "Failed to link $filename to ~/$filename; do you have the right permissions?"
-    cd - > /dev/null
-}
-
-linkdotfile .vimrc
-linkdotfile .vimplainrc
-linkdotfile .tmux.conf
+echo About to nuke your dotfiles for tmux and vim
+read -r -p "Is that OK? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        for f in core/*; do
+            dest=~/.$(basename $f)
+            ln -sf $srcdir/$f $dest && echo Linked $srcdir/$f to $dest
+        done
+        ;;
+    *)
+        echo skipping dotfiles
+        ;;
+esac
