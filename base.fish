245fe53d7451c15d7ca08a441a6e73fbcddeccb3
cqsd <>
2018-07-17 19:59:20 -0700

    thx

--- base.fish
+++ base.fish
@@ -1,21 +1,9 @@
 alias e="vim"
 alias l="ls"
-alias la="ls -a"
-alias ll="ls -l"
-alias topm="top -o mem"
-alias tree="tree -C"
-alias treel="tree -L 2"
-
-# I'm going to kms if I have to keep typing this out in full
-set FISH_HOME $HOME/.config/fish/
 
 function edit_fish_conf
-  e $FISH_HOME/config.fish
+  e $HOME/config/fish/config.fish
 end
-
-# hide the vi indicator...
-# TODO changing modes should change the bg color of the whole prompt
-function fish_mode_prompt; end
 
 function fish_prompt
   printf "%s%s|%s%s \$ " (set_color GREEN) (whoami) (set_color BLUE) (prompt_pwd)
