c2f130a395abf54b3382025cad0d867185fe1a02
cqsd <>
2019-02-06 22:20:09 -0800

    check if in ssh

--- base.fish
+++ base.fish
@@ -6,5 +6,6 @@ function edit_fish_conf
 end
 
 function fish_prompt
-  printf "%s%s|%s%s \$ " (set_color GREEN) (whoami) (set_color BLUE) (prompt_pwd)
+  set name_color (test -z $SSH_CLIENT; and echo (set_color GREEN); or echo (set_color RED))
+  printf "%suid=%s%s|%s \$ " $name_color (id -u) (set_color BLUE) (prompt_pwd)
 end
