a9c9609ee0f063cec72b1455cf560837215b0aa7
cqsd <>
2021-09-09 15:21:35 -0700

    fish: better prompt + cleanup

--- base.fish
+++ base.fish
@@ -9,33 +9,47 @@ function venv-start
   . ~/.venvs/$1/bin/activate.fish
 end
 
-function __git_branch
-  if set branch_name (git symbolic-ref --short head 2>/dev/null)
-    echo -n " ^$branch_name"
-  end
-end
-
 function fish_prompt
-  if [ -z $SSH_CLIENT ]; and not [ (id -u) -eq 0 ]
-    set name_color (set_color GREEN)
+  set _status $status
+
+  # command subtitution seems to hang if the fn prints nothing.  since some
+  # things like aws_session and git branch aren't always printed, we put this
+  # in a variable first. empty variable subtitution is fine
+
+  set prompt_date (set_color RED)(date "+%m/%d %H:%M:%S")
+
+  if [ $_status -ne 0 ]
+    set prompt_prompt ' '(set_color RED)'('$_status') '(set_color GREEN)"\$ "
   else
-    set name_color (set_color RED)
+    set prompt_prompt ' '(set_color GREEN)"\$ "
   end
 
-  if not [ -z $AWS_SESSION_TOKEN ];
-      # fish is suuuuuuuuuuuch shit for this kind of stuff
-      set session_name $AWS_VAULT
-      set -q session_name
-      and set session_name "aws="$session_name"|"
-      or set session_name "aws=unknown|"
+  # name overload
+  set prompt_pwd_ (set_color GREEN)(prompt_pwd)
+
+  if [ -z $AWS_SESSION_TOKEN ]
+      set prompt_aws_session ""
+  else
+    set prompt_aws_session $AWS_VAULT
+    set -q prompt_aws_session
+    or set prompt_aws_session unknown
+    set prompt_aws_session ' '(set_color YELLOW)aws:$prompt_aws_session
   end
 
-  printf "%s%suid=%s|%s%s%s%s%s \$ " \
-    $session_name \
-    (id -u) \
-    (set_color BLUE) \
-    (prompt_pwd) \
-    (set_color RED) \
-    (__git_branch) \
-    (set_color BLUE)
+  if set git_branch_name (git symbolic-ref --short head 2>/dev/null)
+    set prompt_git_branch (set_color cyan)' 'git:$git_branch_name
+  end
+
+  printf "%s%s%s\n%s%s" \
+    $prompt_date \
+    $prompt_git_branch \
+    $prompt_aws_session \
+    $prompt_pwd_ \
+    $prompt_prompt
 end
+
+# fix esc+. not working (grace period is too short by default)
+set -g fish_escape_delay_ms 300
+
+set supplemental ~/.config/fish/supplemental.fish
+test -f $supplemental; and source $supplemental
